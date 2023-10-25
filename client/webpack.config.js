a98857553cee98e4ae43d0579dcaea1d66ef5f1c
KDonnelly33 <kevin.donnelly3@gmail.com>
2023-10-24 20:47:17 -0500

    added code to webpack.config to add service worker and manifest file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,57 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      // added html webpack plugin
+      new HtmlWebpackPlugin({
+        template: '.index.html',
+        title: 'Text Editor',
+      }),
+      // added inject manifest to generate service worker
+      new InjectManifest({
+        swSrc: path.resolve(__dirname, 'src-sw.js'),
+        swDest: 'service-worker.js',
+      }),
+      // added webpack pwa manifest to generate manifest.json
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Text Editor PWA App',
+        short_name: 'Text-Editor',
+        description: 'An applicantion that allows you to edit text files online or offline',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        start_url: '/',
+        icons: [
+          {
+            src: path.resolve('assets/icon-512x512.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
       
     ],
 
     module: {
+      // added rules for css and babel
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {presets: ['@babel/preset-env'] ,},
+          },
+        },
       ],
     },
   };
